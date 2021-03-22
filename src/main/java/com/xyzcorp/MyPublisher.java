b27f61d12fed1936308906578cdfa9f0c91c76ee
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-22 12:24:34 -0600

    update publisher to work better

--- src/main/java/com/xyzcorp/MyPublisher.java
+++ src/main/java/com/xyzcorp/MyPublisher.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class MyPublisher implements Publisher<Long> {
-    private ExecutorService executorService;
+    private final ExecutorService executorService;
 
     public MyPublisher(ExecutorService executorService) {
         this.executorService = executorService;
@@ -17,26 +17,36 @@ public class MyPublisher implements Publisher<Long> {
 
     @Override
     public void subscribe(Subscriber<? super Long> subscriber) {
-        AtomicBoolean atomicDone = new AtomicBoolean(false);
-        AtomicLong counter = new AtomicLong(0L);
-
         subscriber.onSubscribe(new Subscription() {
+            final AtomicBoolean done = new AtomicBoolean(false);
+            final AtomicBoolean started = new AtomicBoolean(false);
+            final AtomicLong counter = new AtomicLong(0L);
+            final AtomicLong requests = new AtomicLong(0L);
+
             @Override
             public void request(long n) {
-                if (n < 0)
-                    subscriber.onError(
-                        new Throwable("Request can't be less than 0"));
+                requests.addAndGet(n);
+                if (!started.get()) {
+                    startLoop();
+                    started.set(true);
+                }
+            }
+
+            private void startLoop() {
                 executorService.submit(() -> {
-                    long goal = counter.get() + n;
-                    while (!atomicDone.get() && counter.get() < goal) {
-                        subscriber.onNext(counter.getAndIncrement());
+                    while (!done.get()) {
+                        if (requests.decrementAndGet() >= 0) {
+                            subscriber.onNext(counter.incrementAndGet());
+                        }
                     }
+                    subscriber.onComplete();
+                    started.set(false);
                 });
             }
 
             @Override
             public void cancel() {
-                atomicDone.set(true);
+                done.set(true);
             }
         });
     }
