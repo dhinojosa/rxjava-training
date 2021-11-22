de761dc9406498779f79c8a074d17c8220498d2c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-11-22 09:30:41 -0700

    Adding reduce and concat, and the big exercise!

--- src/test/java/com/xyzcorp/instructor/ObservableTest.java
+++ src/test/java/com/xyzcorp/instructor/ObservableTest.java
@@ -1,11 +1,18 @@
 package com.xyzcorp.instructor;
 
+import com.xyzcorp.Employee;
+import com.xyzcorp.Manager;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -104,6 +111,107 @@ public class ObservableTest {
         Thread.sleep(1000);
         localDateTimeSingle.subscribe(ldt -> debug("S3", ldt));
         Thread.sleep(1000);
+    }
+
+    @Test
+    public void testChallengeNumberOne() {
+        //Create Observable with interval every 1 second
+        //Create from the interval, one fork that maps the interval times 2,
+        //  subscribe and print result
+        //Create from the interval, another fork that will filter all the odds
+        //  subscribe and print result
+        //Use doOnNext or any of the doOn.... as needed.
+        //You will have to use Thread.sleep(20000) towards the end.
+    }
+
+    @Test
+    public void testFlatMap() throws InterruptedException {
+        Observable<Long> mergeMap =
+            Observable.interval(1, TimeUnit.SECONDS).take(5).flatMap(x -> Observable.just(-x, x, x + 1));
+
+        Observable<Long> concatMap =
+            Observable.interval(1, TimeUnit.SECONDS).take(5).concatMap(x -> Observable.just(-x, x,
+                x + 1));
+
+        mergeMap.subscribe();
+        System.out.println("-------");
+        concatMap.subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testMerge() throws InterruptedException {
+        Observable<String> o1 =
+            Observable.interval(1, TimeUnit.SECONDS).map(i -> "S1" + i);
+
+        Observable<String> o2 =
+            Observable.interval(1, TimeUnit.SECONDS).map(i -> "S2" + i);
+
+        o1.mergeWith(o2).subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testConcat() throws InterruptedException {
+        Observable<String> o1 =
+            Observable.interval(1, TimeUnit.SECONDS).map(i -> "S1: " + i).take(4);
+
+        Observable<String> o2 =
+            Observable.interval(1, TimeUnit.SECONDS).map(i -> "S2: " + i);
+
+        o1.concatWith(o2).subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testReduce() {
+        Single<Integer> reduce =
+            Observable.range(1, 5)
+                      .reduce(1, (subtotal, next) -> {
+                          System.out.printf("subtotal: %d, next: %d\n", subtotal, next);
+                          return subtotal * next;
+                      });
+        reduce.subscribe(System.out::println, Throwable::printStackTrace);
+    }
+
+    @Test
+    public void testReduceWithMaybe() {
+        Maybe<Integer> reduce =
+            Observable.<Integer>empty()
+                      .reduce((subtotal, next) -> {
+                          System.out.printf("subtotal: %d, next: %d\n", subtotal, next);
+                          return subtotal * next;
+                      });
+        reduce.subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("Nothing to report"));
+    }
+
+    @Test
+    public void testFlatMap2() {
+        List<Employee> jkRowlingsEmployees =
+            Arrays.asList(
+                new Employee("Harry", "Potter", 30000),
+                new Employee("Hermione", "Granger", 32000),
+                new Employee("Ron", "Weasley", 32000),
+                new Employee("Albus", "Dumbledore", 40000));
+
+        Manager jkRowling =
+            new Manager("J.K", "Rowling", 46000, jkRowlingsEmployees);
+
+        List<Employee> georgeLucasEmployees =
+            Arrays.asList(
+                new Employee("Luke", "Skywalker", 33000),
+                new Employee("Princess", "Leia", 36000),
+                new Employee("Han", "Solo", 36000),
+                new Employee("Lando", "Calrissian", 41000));
+
+        Manager georgeLucas =
+            new Manager("George", "Lucas", 46000, georgeLucasEmployees);
+
+
+        Observable<Manager> managerObservable = Observable.just(georgeLucas, jkRowling);
     }
 }
 
