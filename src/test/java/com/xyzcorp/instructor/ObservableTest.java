cd22495a59517c9c3671fdfff48046c3d3c2b175
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-11-22 07:26:40 -0700

    Adding defer

--- src/test/java/com/xyzcorp/instructor/ObservableTest.java
+++ src/test/java/com/xyzcorp/instructor/ObservableTest.java
@@ -1,11 +1,14 @@
 package com.xyzcorp.instructor;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ObservableTest {
     @Test
@@ -14,17 +17,96 @@ public class ObservableTest {
         //CMD+OPT+V = Introduce Variable
 
         Observable<Long> longObservable = Observable.<Long>create(emitter -> {
-            emitter.onNext(10L);
-            emitter.onNext(40L);
-            emitter.onNext(50L);
-            emitter.onComplete();
+            Runnable runnable = () -> {
+                emitter.onNext(10L);
+                emitter.onNext(40L);
+                emitter.onNext(50L);
+                emitter.onComplete();
+            };
+            new Thread(runnable).start();
         });
 
-        longObservable.subscribe(x -> System.out.println("S1:" + x));
-        longObservable.subscribe(x -> System.out.println("S2:" + x));
+        longObservable.subscribe(x -> debug("S1", x));
+        longObservable.subscribe(x -> debug("S2", x));
     }
 
     public <A> void debug(String label, A a) {
-        System.out.printf("%s: %s - %s", label, a, Thread.currentThread());
+        System.out.printf("%s: %s - %s\n", label, a, Thread.currentThread());
+    }
+
+    @Test
+    public void testObservableJust() throws InterruptedException {
+        Observable<String> afterScheduler =
+            Observable.just(10, 50, 40)
+                      .subscribeOn(Schedulers.computation())
+                      .map(x -> x * 5)
+                      .subscribeOn(Schedulers.io()) //ignored
+                      .map(String::valueOf);
+        afterScheduler.subscribe(x -> debug("S1", x));
+        afterScheduler.subscribe(x -> debug("S2", x));
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testDifferentWaysToCreateObservableOrFlowable() {
+
+        //Futures are not lazy
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        Observable<Integer> integerObservable =
+            Observable.fromFuture(executorService.submit(() -> 40 * 5));
+    }
+
+    @Test
+    public void testLazinessWithCallable() {
+        Observable<Long> observable =
+            Observable.fromCallable(() -> {
+                System.out.println("We are ready to calculate");
+                return 50 * 10L;
+            });
+
+        System.out.println("This should be the first message");
+
+        observable.subscribe(x -> debug("S1", x));
+    }
+
+    @Test
+    public void testSingleShouldWorkSameWay() {
+        Single<Long> singleObservable =
+            Single.fromCallable(() -> {
+                System.out.println("We are ready to calculate");
+                return 50 * 10L;
+            });
+
+        System.out.println("This should be the first message");
+
+        singleObservable
+            .subscribe(x -> debug("S1", x));
+    }
+
+    @Test
+    public void testInterval() throws InterruptedException {
+        Observable.interval(1, TimeUnit.SECONDS)
+                  .doOnNext(i -> debug("L1", i))
+                  .map(i -> i * 3)
+                  .doOnNext(i -> debug("L2", i))
+                  .subscribe(i -> debug("On Next:", i));
+        Thread.sleep(10000);
+    }
+
+    @Test
+    public void testDefer() throws InterruptedException {
+        Single<LocalDateTime> localDateTimeSingle =
+            Single.just(LocalDateTime.now());
+
+        localDateTimeSingle.subscribe(ldt -> debug("S1", ldt));
+        Thread.sleep(1000);
+        localDateTimeSingle.subscribe(ldt -> debug("S2", ldt));
+        Thread.sleep(1000);
+        localDateTimeSingle.subscribe(ldt -> debug("S2", ldt));
+        Thread.sleep(1000);
     }
 }
+
+
+
+
